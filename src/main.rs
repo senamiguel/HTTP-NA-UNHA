8b126b1ab6b5b03458fd0bc34c351868f8bfc050
Miguel Sena <miguelaugustosena@gmail.com>
2026-06-24 23:22:55 -0300

    feat: implementa leitura de fluxo de dados (raw stream) com buffer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::net::{TcpListener};
 #[allow(unused)]
+use std::net::{TcpListener};
+use std::io::prelude::*;
 
-fn main() {
+
+fn main(){
     let listener = match TcpListener::bind("127.0.0.1:6769"){
         Ok(value) => {
                                     println!("Listening on port 6769");
@@ -9,10 +11,21 @@ fn main() {
         },
         Err(_) => {
                     println!("Error trying to open LISTENING socket"); 
-                    return},
+                    return 
+                },
     };
     for stream in listener.incoming() {
-        println!("Recebinha da silva!")
+        if let Ok(mut stream) = stream {
+            println!("Connected with: {}",stream.peer_addr().unwrap());
+
+            let mut buffer = [0;1024];
+            loop {
+                let bytes_read  = stream.read(&mut buffer).unwrap();
+                if bytes_read == 0 {break;}
+                let data = String::from_utf8_lossy(&buffer[..bytes_read]);
+                println!("{}",data);
+            }
+            println!("Connection ended by client.")
+        }
     }   
-    
 }
